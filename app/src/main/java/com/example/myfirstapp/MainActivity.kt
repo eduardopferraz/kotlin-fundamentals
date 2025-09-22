11d3897e3613eb3c6d5a66901bdcca1f8e304db4
edupferraz <edupferraz@gmail.com>
2025-09-22 19:23:53 -0300

    feat: composition and maintenance of state

--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -8,12 +8,23 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBars
+import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
@@ -33,7 +44,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyFirstAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) {
-                    ArrangementColumns()
+                    HydrationWithState()
                 }
             }
         }
@@ -42,45 +53,69 @@ class MainActivity : ComponentActivity() {
 
 
 @Composable
-fun ArrangementColumns(modifier: Modifier = Modifier) {
-    Column (
-        modifier = modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Top,
+fun Hydration(modifier: Modifier = Modifier) {
 
+    Surface (
+       modifier = modifier
+           .padding(16.dp)
+           .windowInsetsPadding(WindowInsets.statusBars)
     ) {
-        Image(
-            painter = painterResource(id = R.drawable.bg),
-            contentDescription = null,
-            contentScale = ContentScale.FillWidth,
-        )
+        Column (
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center
+        ) {
+            var counter by remember { mutableIntStateOf(0) }
 
-        Text(
-            text = "Tênis de mesa",
-            fontSize = 24.sp,
-            modifier = Modifier.padding(16.dp)
-        )
+            Text(
+                modifier = Modifier.padding(vertical = 16.dp),
+                text = "You drunk $counter cups of water.",
+                style = MaterialTheme.typography.headlineSmall
+            )
 
-        Text(
-            text = "O tênis de mesa surgiu na Inglaterra no século XIX imitando o jogo de tênis em um ambiente fechado.",
-            modifier = Modifier
-                .padding(horizontal = 16.dp),
-            textAlign = TextAlign.Justify
-        )
-
-        Text(
-            text = "Uma partida de tênis de mesa é disputada em melhor de qualquer número de sets ímpares. Usualmente, torneios nacionais são disputados em melhor de 5 sets e torneios internacionais em melhor de 7 sets, o que significa que o jogador ou dupla que vencer, respectivamente, 3 ou 4 sets vence a partida. Para vencer um set, o jogador ou a dupla precisa conseguir 11 pontos, ou em caso de empate em 10 pontos, somar dois pontos de vantagem em relação ao seu adversário.",
-            modifier = Modifier
-                .padding(16.dp),
-            textAlign = TextAlign.Justify
-        )
-
+            Button(
+                modifier = Modifier.fillMaxWidth(),
+                onClick = { counter++ },
+                enabled = counter < 10
+            ) {
+                Text(
+                    text = "Drink water",
+                    style = MaterialTheme.typography.headlineSmall
+                )
+            }
+        }
     }
-
-
 }
+
+@Composable
+fun HydrationWithoutState(
+    counter: Int,
+    onValueChange: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(modifier = modifier.padding(16.dp)) {
+        Text("You drunk $counter cups today!")
+        Button(
+            onClick = { onValueChange() }, enabled = counter < 10
+        ) {
+            Text("Drink")
+        }
+    }
+}
+
+@Composable
+fun HydrationWithState(modifier: Modifier = Modifier) {
+    var waterCounter by rememberSaveable { mutableIntStateOf(0) }
+    var juiceCounter by rememberSaveable { mutableIntStateOf(0) }
+    Column(modifier= modifier.fillMaxSize()) {
+        HydrationWithoutState(counter = waterCounter, onValueChange = { waterCounter++ })
+        HydrationWithoutState(counter = juiceCounter, onValueChange = { juiceCounter++ })
+    }
+}
+
+
 
 @Preview(showBackground = true)
 @Composable
-private fun FundoPreview() {
-    ArrangementColumns()
+private fun HydrationPreview() {
+    HydrationWithState()
 }
